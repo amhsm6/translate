715c9e4f6ce2bbd813a677fb34ba8a92e5c41a3a
anton <amhsm6@gmail.com>
2024-12-08 22:01:40 +0200

    continue frontend

---
+++ client/app/document/[documentId]/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function updateTarget(id: string | null, text: string) {
+    console.log('I am a server action: ', id)
+}
--- client/app/document/[documentId]/components/Segment.tsx
+++ client/app/document/[documentId]/components/Segment.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import type { Segment } from "../context";
+import { updateTarget } from "../actions";
 import { Button } from "@/components/ui/button";
+import { HashLoader } from "react-spinners";
 
 type Props = {
     segment: Segment
 };
 
 export default function Segment({ segment }: Props) {
-    const [state, setState] = useState('');
+    const [targetText, setTargetText] = useState('');
+    const [saving, setSaving] = useState(false);
+
     const ref = useRef<HTMLTextAreaElement>(null);
 
     // FIXME: a bit awkward but warks
@@ -15,12 +19,21 @@ export default function Segment({ segment }: Props) {
         if (!ref.current) { return; }
 
         const cols = ref.current.cols + 1;
-        const rows = state.split(/\r\n|\r|\n/)
-            .map(line => { console.log(line, line.length, 1 + Math.floor(line.length / cols)); return 1 + Math.floor(line.length / cols); })
+        const rows = targetText.split(/\r\n|\r|\n/)
+            .map(line => 1 + Math.floor(line.length / cols))
             .reduce((x, y) => x + y, 0);
 
         ref.current.rows = rows;
-    }, [state]);
+    }, [targetText]);
+
+    const save = () => {
+        setSaving(true);
+
+        // TODO: execute a server action
+        updateTarget(segment.target?.id, targetText);
+
+        setTimeout(() => setSaving(false), 1000);
+    };
 
     return (
         <div className="p-3 border-b-2 border-b-gray-300 flex">
@@ -34,13 +47,17 @@ export default function Segment({ segment }: Props) {
                 ref={ ref }
                 className="min-h-20 border-2 resize-none"
                 cols={ 30 }
-                value={ state }
-                onChange={ e => setState(e.target.value) }
+                value={ targetText }
+                onChange={ e => setTargetText(e.target.value) }
+                disabled={ saving }
             />
 
-            <Button>
-                123
-            </Button>
+            <div className="w-auto flex-grow flex justify-center items-center">
+                <div className="w-5/12 flex justify-between items-center">
+                    <Button onClick={ save } disabled={ saving }>Save</Button>
+                    { saving && <HashLoader size={ 20 } /> }
+                </div>
+            </div>
         </div>
     );
 }
